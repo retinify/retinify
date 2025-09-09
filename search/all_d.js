6208fc9953cefc8126da9d0aabfb9b078f14a1be
SensuiYagi <SensuiYagi@users.noreply.github.com>
2025-09-09 18:41:54 +0000

    deploy: e94cfaaf70b24ea2f6453599f97e933eaa3182ba

--- search/all_d.js
+++ search/all_d.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['index_2emd_0',['index.md',['../index_8md.html',1,'']]],
   ['info_1',['INFO',['../namespaceretinify.html#ad7864f6e39ba12c3d9bd53d13c64ac7fa551b723eafd6a31d444fcb2f5920fbd3',1,'retinify']]],
-  ['initialize_2',['Initialize',['../classretinify_1_1Pipeline.html#a007ea6125af54145eb6730c400fd7d37',1,'retinify::Pipeline']]],
+  ['initialize_2',['Initialize',['../classretinify_1_1Pipeline.html#a39b62fdc1a7db64f771ff1ead330074f',1,'retinify::Pipeline']]],
   ['install_20opencv_3',['1. Install OpenCV',['../tutorials.html#autotoc_md7',1,'']]],
   ['install_20retinify_4',['install retinify',['../tutorials.html#autotoc_md8',1,'2. Install retinify'],['../installation.html#autotoc_md3',1,'3. Install retinify']]],
   ['installation_5',['installation',['../installation.html#autotoc_md5',1,'4. Verify Installation'],['../installation.html',1,'Installation']]],
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['initialize_0',['Initialize',['../classretinify_1_1Pipeline.html#a007ea6125af54145eb6730c400fd7d37',1,'retinify::Pipeline']]],
+  ['initialize_0',['Initialize',['../classretinify_1_1Pipeline.html#a39b62fdc1a7db64f771ff1ead330074f',1,'retinify::Pipeline']]],
   ['isok_1',['IsOK',['../classretinify_1_1Status.html#ab4df7a7e3c4c8bd9ba8da837f636421b',1,'retinify::Status']]]
 ];
